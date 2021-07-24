39ab78801818b86f75ed678140db91a39c0149d7
sjkingsj <tmdwjd1210@naver.com>
2021-07-24 19:58:50 +0900

    Bamsongi Assets

---
+++ Assets/BamsongiController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiController : MonoBehaviour
+{
+    // 매개변수 방향으로 힘을 가하기
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    // 충돌을 감지하면
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true; // 움직임을 멈추기
+        GetComponent<ParticleSystem>().Play(); // 파티클 재생
+    }
+
+    void Start()
+    {
+        // +Z축 방향 벡터를 매개변수로 전달, 중력으로 낙하를 방지하기 위해 Y축 방향 힘 가하기
+        // Shoot(new Vector3(0, 200, 2000));
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/BamsongiGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiGenerator : MonoBehaviour
+{
+    // 아웃렛 접속을 사용해 대입할 수 있도록 public 붙여서 선언 
+    public GameObject bamsongiPrefab;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // 화면을 누르면
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 밤송이 인스턴스 만들기
+            GameObject bamsongi = Instantiate(bamsongiPrefab) as GameObject;
+            // 카메라에서 탭 좌표로 향하는 벡터에 따른 Ray 클래스 반환, 좌표 전달
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            // 탭 좌표로 향하는 벡터를 normalized 변수로 길이가 1 * 2000으로 Shoot 매서드 호출
+            bamsongi.GetComponent<BamsongiController>().Shoot(worldDir.normalized * 2000);
+            // 날리고자 하는 방향의 벡터를 전달하고 Shoot 매서드 호출
+            // bamsongi.GetComponent<BamsongiController>().Shoot(new Vector3(0, 200, 2000));
+        }
+    }
+}
